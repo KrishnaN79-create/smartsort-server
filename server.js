1ff73cea08647875a213b582e39fa3c1807a1749
KrishnaN79-create <krishnanatarajan79@gmail.com>
2026-03-13 17:20:21 -0400

    Refactor server.js to handle multiple gemini endpoints

--- server.js
+++ server.js
@@ -1,22 +1,85 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.post("/gemini", async (req, res) => {
+// const corsOptions = {
+//     origin: ['http://localhost:5500'], 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] 
+// };
+
+// app.use(cors(corsOptions));
+
+app.use(cors());
+
+app.post("/gemini-text", async (req, res) => {
   const { prompt } = req.body;
 
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" +
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=" +
       process.env.GEMINI_API_KEY,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         contents: [{ parts: [{ text: prompt }] }],
+      }),
+    }
+  );
+
+  const data = await response.json();
+  res.json(data);
+});
+
+
+app.post("/gemini-image", async (req, res) => {
+  const { prompt, inlineData } = req.body;
+
+  console.log("Received prompt:", prompt);
+  console.log("Received inlineData:", inlineData );
+  const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=" +
+      process.env.GEMINI_API_KEY,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                { text: prompt },
+                {
+                  inlineData: inlineData
+                }
+              ]
+            }
+          ]
+        })
+
+    }
+  );
+
+  const data = await response.json();
+  res.json(data);
+});
+
+
+app.post("/gemini-file", async (req, res) => {
+  const { prompt, imagePart } = req.body;
+
+  const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=" +
+      process.env.GEMINI_API_KEY,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        contents: [{ parts: [{ text: prompt }, imagePart] }],
       }),
     }
   );
